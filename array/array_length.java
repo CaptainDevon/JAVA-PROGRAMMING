79522f430d8c1530a7792a4026fd4d54787e2f6a
CaptainDevon <divyansh2004ind@gmail.com>
2024-06-06 12:26:57 +0530

    day 3

---
+++ array/array_length.java
@@ -0,0 +1,9 @@
+package array;
+
+public class array_length {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,54,655,44,0,33};
+        System.out.println(arr.length);
+    }
+    
+}
