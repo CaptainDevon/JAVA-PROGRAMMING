914790368155ed14cfc3c3b1208adc1bcc605c94
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-18 21:56:30 +0530

    java patterns

---
+++ Fundamentals/Loops.java
@@ -0,0 +1,22 @@
+public class Loops {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++)
+        {
+            System.out.println("For Loop");
+        }
+
+        int j=0;
+        while(j<5)
+        {
+            System.out.println("While Loop");
+            j++;
+        }
+
+        int k=0;
+        do
+        {
+            System.out.println("Do While Loop");
+            k++;
+        }while (k<5);
+    }
+}
