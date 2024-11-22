2bd1095f6068ed8a3a29e92ecc59ffeabb3f8fc8
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-22 11:46:19 +0530

    infinte loop using do while

---
+++ Fundamentals/Exercise Questions/Question6.java
@@ -0,0 +1,10 @@
+public class Question6 {
+    public static void main(String args[])
+    {
+        int n=2;
+        do{
+            System.out.println("infinite loop");
+            n++;
+        }while(n>1);
+    }
+}
