90aaeb1800346f407567b14788c4aa325e343150
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-28 11:28:31 +0530

    .

--- Data Structures and Algorithms/Recursion/PrintingSubsequenceOfString.java
+++ Data Structures and Algorithms/Recursion/PrintingSubsequenceOfString.java
@@ -1,19 +1,24 @@
+import java.util.*;
+
 public class PrintingSubsequenceOfString {
-    public static void main(String args[])
-    {
-        String s="abc";
-        subSequence(s, 0, "");
+    public static void main(String args[]) {
+        String s = "abc";
+        HashSet<String> set = new HashSet<>();
+        subSequence(s, 0, "", set);
     }
 
-    public static void subSequence(String s,int index,String newString)
-    {
-        if(index==s.length())
-        {
-            System.out.println(newString);
-            return;
+    public static void subSequence(String s, int index, String newString, HashSet<String> set) {
+        if (index == s.length()) {
+            if (set.contains((newString))) {
+                return;
+            } else {
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
         }
-        char currChar=s.charAt(index);
-        subSequence(s, index+1, newString+currChar);
-        subSequence(s, index+1, newString);
+        char currChar = s.charAt(index);
+        subSequence(s, index + 1, newString + currChar, set);
+        subSequence(s, index + 1, newString, set);
     }
 }
