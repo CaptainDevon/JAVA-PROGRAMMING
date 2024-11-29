3847c1d976e336f6bdc04ffb5af5ad2e56e3331e
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-29 19:13:47 +0530

    compile time polymorphism

---
+++ OOPS concepts/FunctionOverloading.java
@@ -0,0 +1,14 @@
+class People {
+    public void display() {
+        System.out.println("hello I am a Human");
+    }
+
+    public void display(String occupation) {
+        System.out.println("hello I am " + occupation);
+    }
+
+    public void display(String occupation,int age)
+    {
+        System.out.println("Hello I am "+occupation+" And I am "+age+" Year's old");
+    }
+}
--- OOPS concepts/Main.java
+++ OOPS concepts/Main.java
@@ -1,14 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Pen pen1=new Pen();
-        Pen pen2=new Pen();
-        //using of properties
-        pen1.colour="blue";
-        pen1.type="ball";
-        
-        pen2.colour="black";
-        pen2.type="gel";
-        //calling of methods
+        Pen pen1 = new Pen();
+        Pen pen2 = new Pen();
+        // using of properties
+        pen1.colour = "blue";
+        pen1.type = "ball";
+
+        pen2.colour = "black";
+        pen2.type = "gel";
+        // calling of methods
         pen1.write();
         pen1.printColour();
         pen1.printType();
@@ -16,15 +16,21 @@ public class Main {
         pen2.printColour();
         pen2.printType();
 
-        //non parameterized constructor
-        Student s1=new Student();
+        // non parameterized constructor
+        Student s1 = new Student();
         s1.studentinfo();
 
-        //parameterized constructor
-        Student s2=new Student("diyansh Sinha",20);
+        // parameterized constructor
+        Student s2 = new Student("diyansh Sinha", 20);
         s2.studentinfo();
 
-        Student s3=new Student(s2);
+        Student s3 = new Student(s2);
         s3.studentinfo();
+
+        System.out.println("-------------------------------------------------------------------------------");
+        People p=new People();
+        p.display();
+        p.display("Software engineer");
+        p.display("Software engineer",20 );
     }
 }
