e6b8d07b858f910dd4170d2095ba981880e163e3
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-29 16:31:08 +0530

    classes and objects

---
+++ OOPS concepts/ClassesAndObjects.java
@@ -0,0 +1,22 @@
+class Pen
+{
+    //class data
+    String colour;
+    String type;      //ball pen or gel pen
+
+    //class members
+    public void write()
+    {
+        System.out.println("Writing something");
+    }
+
+    public void printColour()
+    {
+        System.out.println(this.colour);
+    }
+
+    public void printType()
+    {
+        System.out.println(this.type);
+    }
+}
--- OOPS concepts/Main.java
+++ OOPS concepts/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        Pen pen1=new Pen();
+        Pen pen2=new Pen();
+        //using of properties
+        pen1.colour="blue";
+        pen1.type="ball";
         
+        pen2.colour="black";
+        pen2.type="gel";
+        //calling of methods
+        pen1.write();
+        pen1.printColour();
+        pen1.printType();
+        pen2.write();
+        pen2.printColour();
+        pen2.printType();
     }
 }
