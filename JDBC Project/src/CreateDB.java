ea0527caaaf8d2b2e105ad0d6be75011637312e7
CaptainDevon <divyansh2004ind@gmail.com>
2024-11-30 13:59:53 +0530

    jdbc fetching data

--- JDBC Project/src/CreateDB.java
+++ JDBC Project/src/CreateDB.java
@@ -4,16 +4,16 @@ import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 public class CreateDB {
+    private static String url = "jdbc:mysql://localhost:3306/";
+    private static String userName = "root";
+    private static String password = "blabla";   // i know the pass just i not showing 
     public static void main(String[] args) {
         Connection connection = null;
         Statement statement = null;
 
         try {
-            String url = "jdbc:mysql://localhost:3306/";
+                 
             String databaseName = "SampleDb";
-            String userName = "root";
-            String password = "SomePassword";     // I know the password just not showing it 
-
             connection = DriverManager.getConnection(url, userName, password);
 
             String sql = "CREATE DATABASE " + databaseName;
---
+++ JDBC Project/src/SelectStatement.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+//import javax.swing.JOptionPane;
+
+public class SelectStatement {
+    private static String url = "jdbc:mysql://localhost:3306/sampledb";
+    private static String userName = "root";
+    private static String password = "Bla Bla";   // i know the password just not showing
+
+    public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Statement statement = connection.createStatement();
+            String query = "select * from students";
+            ResultSet resultSet = statement.executeQuery(query);
+            System.out.println("   ID   " + "   name   " + "   age    " + "   marks   ");
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                double marks = resultSet.getDouble("marks");
+                System.out.println(
+                        "   " + id + "   " + "   " + name + "   " + "   " + age + "    " + "   " + marks + "   ");
+
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
