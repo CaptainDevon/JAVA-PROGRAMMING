b24a53a1f7b3e82301aa51cdae9e7c146e37a4c8
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-02 14:16:40 +0530

    reversing of array

---
+++ Collections/Algorithms/Reversing.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Reversing {
+    public static void main(String[] args) {
+        ArrayList<Integer>nums=new ArrayList<>(Arrays.asList(1,2,3,4,5));
+        Collections.reverse(nums);
+        System.out.println(nums);
+    }
+}
