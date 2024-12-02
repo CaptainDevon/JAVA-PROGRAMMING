fe29aec280fcd22bd43e631658a1aebcd388a48b
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-02 13:00:46 +0530

    learning Set

---
+++ Collections/LearningSet.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class LearningSet {
+    public static void main(String[] args) {
+        //stores unique values in any order
+        Set<Integer> set=new HashSet<>();
+        set.add(10);
+        set.add(5);
+        set.add(16);
+        set.add(2);
+        set.add(20);
+        System.out.println(set);
+    }   
+}
