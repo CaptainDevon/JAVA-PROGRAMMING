f9ad647f439155f99128981d492b504c87be40f5
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-02 14:18:25 +0530

    finding maximum element

---
+++ Collections/Algorithms/MaximumElement.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class MaximumElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums=new ArrayList<>(Arrays.asList(1,5,3,6,-2,-7,0,8,11));
+        System.out.println(Collections.max(nums));
+    }    
+}
