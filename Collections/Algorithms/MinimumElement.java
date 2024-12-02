eaf2f6b70fd862f8f3e27e8049c104519d73b993
CaptainDevon <divyansh2004ind@gmail.com>
2024-12-02 14:19:04 +0530

    minimum element in array

---
+++ Collections/Algorithms/MinimumElement.java
@@ -0,0 +1,8 @@
+import java.util.*;
+
+public class MinimumElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> nums=new ArrayList<>(Arrays.asList(1,5,3,6,-2,-7,0,8,11));
+        System.out.println(Collections.min(nums));
+    } 
+}
