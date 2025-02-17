332683f3bfbc02a311759925730d72582a704b02
CaptainDevon <divyansh2004ind@gmail.com>
2025-02-18 00:19:49 +0530

    Number Guessing game

---
+++ Game Designing/NumberGuessingGame.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class NumberGuessingGame {
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        Random random=new Random();
+        int computerValue=random.nextInt(10);
+        
+        while(true)
+        {
+            System.out.print("Guess The Number: ");
+            int playerValue=s.nextInt();
+            if(playerValue>computerValue)
+            {
+                System.out.println("Too High!");
+            }
+            else if(playerValue<computerValue)
+            {
+                System.out.println("Too Low!");
+            }
+            else 
+            {
+                System.out.print("You have Guessed the number correctly");
+                break;
+            }
+        }
+
+        s.close();
+    }
+}
